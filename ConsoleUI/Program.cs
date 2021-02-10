f7aa34fbe6cb2c670c8925baf1116a654c0d07fe
mustafasaid99 <mustafaasaid99@gmail.com>
2021-02-11 00:18:23 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,24 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            foreach (var item in productManager.GetByUnitPrice(40,100))
+            {
+                Console.WriteLine(item.ProductName);
+                Console.WriteLine(item.CategoryId);
+                Console.WriteLine(item.ProductName);
+                
+
+            }
+            
+        }
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,60 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfProductDal : IProductDal
+    {
+        public void Add(Product entity)
+        {
+            using (NorthwindContex contex=new NorthwindContex())
+            {
+                var addedEntity = contex.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                contex.SaveChanges();
+            }
+        }
+
+        public void Delete(Product entity)
+        {
+            using (NorthwindContex contex = new NorthwindContex())
+            {
+                var deletedEntity = contex.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                contex.SaveChanges();
+            }
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            using(NorthwindContex context=new NorthwindContex())
+            {
+                return context.Set<Product>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            using (NorthwindContex contex = new NorthwindContex())
+            {
+                return filter == null ? contex.Set<Product>().ToList() : contex.Set<Product>().Where(filter).ToList();
+            }
+        }
+
+        public void Update(Product entity)
+        {
+            using (NorthwindContex contex = new NorthwindContex())
+            {
+                var updatedEntity = contex.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                contex.SaveChanges();
+            }
+        }
+    }
+}
