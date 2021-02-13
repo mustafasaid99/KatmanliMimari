3c4dd68b6635c5dd29176bd02e31711242a0fd2b
mustafasaid99 <mustafaasaid99@gmail.com>
2021-02-13 22:12:39 +0300

    Adım 10

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -27,14 +27,21 @@ namespace ConsoleUI
         private static void ProductExample()
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach (var item in productManager.GetProductDetails())
+            var result = productManager.GetProductDetails();
+            if (result.Success)
             {
-                Console.WriteLine(item.ProductName+"/"+item.CategoryName);
-                
+                foreach (var item in result.Data)
+                {
+                    Console.WriteLine(item.ProductName + "/" + item.CategoryName);
 
-
+                }
 
             }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+          
         }
     }
 }
---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        public Result(bool succcess, string message):this(succcess)
+        {
+            Message = message; 
+        }
+        public Result(bool succcess)
+        {
+            Success = succcess;
+        }
+
+        public bool Success { get; }
+
+        public string Message { get; }
+    }
+}
