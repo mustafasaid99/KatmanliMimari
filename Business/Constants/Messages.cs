4e19c3337b6994319dc6a363fca124dc7d686dee
mustafasaid99 <mustafaasaid99@gmail.com>
2021-02-28 23:46:10 +0300

    Adım 14

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Core.Entities.Concrete;
+using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -13,5 +15,12 @@ namespace Business.Constants
         public static string ProductCountOfCategoryError="Bir kategoride en fazla 10 ürün olabilir";
         public static string ProductNameAlreadyExist ="Bu isimde zaten başka bir ürün var";
         public static string CategoryLimitExceded="Kategori limiti açıldığı için yeni ürün eklenmiyor";
+        public static string AuthorizationDenied="Yetkiniz yok";
+        public static string UserRegistered="Kayıt oldu";
+        public static string UserNotFound ="Kullanıcı bulunamadı";
+        public static string PasswordError="Parola hatası";
+        public static string SuccessfulLogin="Başarılı giriş";
+        public static string UserAlreadyExists="Kullanıcı Mevcut";
+        public static string AccessTokenCreated="Token oluşturuldu";
     }
 }
